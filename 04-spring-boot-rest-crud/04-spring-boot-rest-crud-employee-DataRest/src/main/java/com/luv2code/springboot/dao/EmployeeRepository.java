645420f0bda482ab9ceca67ad3c2260ebea7d12a
Pranay Shinde <spranay740@gmail.com>
2024-11-07 19:08:20 +0530

    rest api using DataRest

--- 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-DataRest/src/main/java/com/luv2code/springboot/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-rest-crud-employee-DataRest/src/main/java/com/luv2code/springboot/dao/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package com.luv2code.springboot.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import com.luv2code.springboot.entity.Employee;
 
+
+//@RepositoryRestResource(path="members") //to change path i.e to http://localhost:8080/members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     
     // Employee => Entity type
@@ -12,3 +16,4 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     
     //thats it no need to write code
 }
+
